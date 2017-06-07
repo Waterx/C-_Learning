f8651ebff223c2059bab73d77ea868bc0b4158eb
waterx <hzx915184874@gmail.com>
2017-06-07 15:29:05 +0800

    基本完成

--- lab-test/stack_and_queue/stack_and_queue/源.cpp
+++ lab-test/stack_and_queue/stack_and_queue/源.cpp
@@ -36,6 +36,11 @@ public:
 	int MakeEmpty();
 	int Full();
 	int Length();
+	void Read();
+	int Write();
+	int WrToFile();
+	int ReadfrFile();
+	void Display();
 };
 
 int Stack::Add(int data) {
@@ -73,7 +78,11 @@ int Stack::Remove() {
 
 }
 int Stack::Get() {
-	return rear->data;
+	if (rear) {
+		return rear->data;
+	}
+	else
+		return -1;
 }
 int Stack::Put(int data) {
 	Node* t = new Node(data, rear);
@@ -93,9 +102,71 @@ int Stack::Full() {
 }
 int Stack::Length() {
 	return numOfNode;
+}
+void Stack::Read() {
+	Node* t = rear;
+	for (int i = 0; i < numOfNode; i++) {
+		cout << t->data << " ";
+		t = t->next;
+	}
+	cout << endl;
+}
+int Stack::Write() {
+	cout << "Stack input numbers:";
+	do {
+		int t;
+		cin >> t;
+		Add(t);
+	} while (getchar() != '\n');
 	return 1;
 }
+int Stack::WrToFile() {
+	ofstream output("stack.txt");
+	if (head) {
+		Stack s;
+		Node* t = rear;
+		for (int i = 0; i < numOfNode; i++) {
+			s.Add(t->data);
+			t = t->next;
+		}
+		t = s.rear;
+		for (int i = 0; i < numOfNode; i++) {
+			output << t->data << " ";
+			t = t->next;
+		}
+		return 1;
+	}
+	return 0;
+}
+int Stack::ReadfrFile() {
+	ifstream input("stack.txt");
+	if (input.fail()||Full()) {
+		return -1;
+	}
+	MakeEmpty();
+	/*while (!input.eof()) {
+		int t;
+		input >> t;
+		Add(t);
+	}*/
+	while (true) {
+		int x;
+		input >> x;
+		if (input.eof()) break;
+		Add(x);
+	}
+	return 1;
 
+}
+void Stack::Display() {
+	cout << "--STACK--" << endl;
+	cout << "Elements:";
+	Read();
+	cout << "Top:" << Get() << endl;
+	cout << "Length:" << Length() << endl;
+	cout << "IsEmpty:" << Full() << endl;
+	cout << endl;
+}
 class Queue :public Bag {
 private:
 	int numOfNode;
@@ -109,6 +180,11 @@ public:
 	int MakeEmpty();
 	int Full();
 	int Length();
+	void Read();
+	int Write();
+	int WrToFile();
+	int ReadfrFile();
+	void Display();
 };
 
 int Queue::Add(int data) {
@@ -129,15 +205,10 @@ int Queue::Add(int data) {
 
 int Queue::Remove() {
 	Node* t = head;
-	Node* beforerear;
-	int num = numOfNode;
 	if (numOfNode > 1) {
-		for (int i = 0; i < num - 2; i++) {
-			t = t->next;
-		}
-		beforerear = t;
-		t = t->next;
-		numOfNode--;        //×öµ½Õâ
+		head = head->next;
+		delete t;
+		numOfNode--;        
 		return 1;
 	}
 	else if (numOfNode == 1) {
@@ -152,16 +223,94 @@ int Queue::Remove() {
 	}
 
 }
+int Queue::Get() {
+	if (head) {
+		return head->data;
+	}
+	else {
+		return -1;
+	}
+}
+int Queue::Put(int data) {
+	Node* t = new Node(data, 0);
+	rear->next = t;
+	rear = t;
+	numOfNode++;
+	return 1;
+}
+int Queue::MakeEmpty() {
+	int num = numOfNode;
+	for (int i = 0; i < num; i++) {
+		Remove();
+	}
+	return 1;
+}
+int Queue::Full() {
+	return head == 0;
+}
+int Queue::Length() {
+	return numOfNode;
+}
+void Queue::Read() {
+	Node* t = head;
+	for (int i = 0; i < numOfNode; i++) {
+		cout << t->data << " ";
+		t = t->next;
+	}
+	cout << endl;
+}
+int Queue::Write() {
+	cout << "Queue input numbers:";
+	do {
+		int t;
+		cin >> t;
+		Add(t);
+	} while (getchar() != '\n');
+	return 1;
+}
+int Queue::WrToFile() {
+	ofstream output("queue.txt");
+	if (head) {
+		Node *t = head;
+		for (int i = 0; i < numOfNode; i++) {
+			output << t->data << endl;
+			t = t->next;
+		}
+		return 1;
+	}
+	return 0;
+}
+int Queue::ReadfrFile() {
+	ifstream input("queue.txt");
+	if (input.fail() || Full()) {
+		return -1;
+	}
+	MakeEmpty();
+	while (true) {
+		int x;
+		input >> x;
+		if (input.eof()) break;
+		Add(x);
+	}
+	return 1;
+}
+void Queue::Display() {
+	cout << "--QUEUE--" << endl;
+	cout << "Elements:";
+	Read();
+	cout << "Head:" << head->data << endl;
+	cout << "Rear:" << rear->data << endl;
+	cout << "Length:" << Length() << endl;
+	cout << "IsEmpty:" << Full() << endl;
+	cout << endl;
+}
 
 int main() {
 	Stack s;
-	s.Add(4);
-	s.Add(6);
-	s.Add(9);
-	//s.Remove();
-	//s.Remove();
-
-	//s.Remove();
-	s.MakeEmpty();
+	s.Write();
+	s.Display();
+	Queue q;
+	q.Write();
+	q.Display();
 	return 0;
 }
